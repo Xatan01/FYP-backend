bea2231bf0f49f1bb335fbf71660279e1ffe43b5
Xatan01 <xaviertan2001@gmail.com>
2026-03-01 18:36:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routes import auth
 from routes import lesson
+from routes import market
 from routes import quiz
 from routes import market
 from routes import market
